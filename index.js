2788027b6b91d32291c456bd0ca0ea12c69537c3
Liza <liza.shulenko@gmail.com>
2019-11-24 15:34:15 +0200

    add all algorithms

--- index.js
+++ index.js
@@ -6,9 +6,8 @@ const port = 3000;
 app.get('/', (request, response) => {
     response.sendFile(path.join(__dirname+'/index.html'));
 })
-app.get('/algorithms.js',function(req,res) {
-    res.sendFile(path.join(__dirname + '/algorithms.js'));
-});
+app.use(express.static(__dirname));
+
 app.listen(port, (err) => {
     if (err) {
         return console.log('something bad happened', err)
