0504d10102f98936b2bfa5ae4d2531fdabac82c2
AlexWebUa <alexwebua1@gmail.com>
2019-12-10 01:06:54 +0200

    Added "Push me!" button

--- js/animator.js
+++ js/animator.js
@@ -1,4 +1,6 @@
+const shuffleButton = document.getElementById("shuffle");
 const test = document.getElementById("test");
+const arrayControlButtons = document.getElementById("control-size").getElementsByClassName("btn-group")[0].getElementsByClassName("btn");
 
 test.addEventListener("click", ev => {
     testAll(20);
